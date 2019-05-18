395e70bc34fd13d04242fd52da66722cddcbc143
chenguang du <duchengaung@licaigc.com>
2019-05-19 06:43:26 +0800

    add setup info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="pyanalysis",
+    version="1.0",
+    author="strengthening",
+    author_email="strengthen2010@gmail.com",
+    # url='http://www.you.com/projectname',
+    packages=["pyanalysis"],
+)
