3ba042eed551b484c741165096a01bb4c7a04414
chenguang du <duchengaung@licaigc.com>
2019-09-20 14:59:56 +0800

    update the dockerfile

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name="pyanalysis",
-    version="2.0",
+    version="2.0.0",
     author="strengthening",
-    author_email="strengthen2010@gmail.com",
+    author_email="ducg@foxmail.com",
     # url='http://www.you.com/projectname',
-    packages=["pyanalysis", "pyanalysis.database", "pyanalysis.log"],
+    packages=["pyanalysis"],
 )
